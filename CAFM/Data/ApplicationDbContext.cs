d39524d19e844b60845e0d4349ec3ed2e510a0de
Carlosfrndzmndz <carlosafernandez@uade.edu.ar>
2022-12-07 23:12:25 -0300

    update

--- CAFM/Data/ApplicationDbContext.cs
+++ CAFM/Data/ApplicationDbContext.cs
@@ -9,11 +9,20 @@ namespace CAFM.Data
         {
         }
 
-        public DbSet<Usuario> Usuario { get; set; }
-        public DbSet<Rol> Rol { get; set; }
-        public DbSet<PermisosRol> PermisosRol { get; set; }
-        public DbSet<RolesUsuario> RolesUsuario { get; set; }
-        public DbSet<Permiso> Permiso { get; set; }
+        public DbSet<AperturaControlAcceso> AperturaControlAcceso { get; set; }
         public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Contacto> Contacto { get; set; }
+        public DbSet<ContactoClientes> ContactoClientes { get; set; }
+        public DbSet<ControlesAccesoClientes> ControlesAccesoClientes { get; set; }
+        public DbSet<Permiso> Permiso { get; set; }
+        public DbSet<PermisosRol> PermisosRol { get; set; }
+        public DbSet<Plan> Plan { get; set; }
+        public DbSet<ReconocimientoControlAcceso> ReconocimientoControlAcceso { get; set; }
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<RolesUsuario> RolesUsuario { get; set; }
+        public DbSet<Servicio> Servicio { get; set; }
+        public DbSet<ServiciosPlan> ServiciosPlan { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        
     }
 }
---
+++ CAFM/Migrations/20221208015547_CreateTables071220222255.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CAFM.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTables071220222255 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Direccion",
+                table: "Cliente",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdPlan",
+                table: "Cliente",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Localidad",
+                table: "Cliente",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pais",
+                table: "Cliente",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Provincia",
+                table: "Cliente",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "AperturaControlAcceso",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AperturaControlAcceso", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Celular = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacto", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactoClientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    IdContacto = table.Column<int>(type: "int", nullable: false),
+                    Cargo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactoClientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ControlesAccesoClientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    IdReconocimiento = table.Column<int>(type: "int", nullable: false),
+                    IdApertura = table.Column<int>(type: "int", nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ControlesAccesoClientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plan",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Precio = table.Column<int>(type: "int", nullable: false),
+                    FechaAlta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaBaja = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plan", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReconocimientoControlAcceso",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReconocimientoControlAcceso", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Servicio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servicio", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiciosPlan",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdPlan = table.Column<int>(type: "int", nullable: false),
+                    IdServicio = table.Column<int>(type: "int", nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiciosPlan", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AperturaControlAcceso");
+
+            migrationBuilder.DropTable(
+                name: "Contacto");
+
+            migrationBuilder.DropTable(
+                name: "ContactoClientes");
+
+            migrationBuilder.DropTable(
+                name: "ControlesAccesoClientes");
+
+            migrationBuilder.DropTable(
+                name: "Plan");
+
+            migrationBuilder.DropTable(
+                name: "ReconocimientoControlAcceso");
+
+            migrationBuilder.DropTable(
+                name: "Servicio");
+
+            migrationBuilder.DropTable(
+                name: "ServiciosPlan");
+
+            migrationBuilder.DropColumn(
+                name: "Direccion",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "IdPlan",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Localidad",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Pais",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Provincia",
+                table: "Cliente");
+        }
+    }
+}
